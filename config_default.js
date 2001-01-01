60d02daf3342acf94355c0d554401a473e65a888
Andrew Lovett-Barron <andrew@readywater.ca>
2000-12-31 19:04:39 -0500

    Numerous changes

---
+++ config_default.js
@@ -0,0 +1,18 @@
+{
+	"uuid":0
+	, "setupMode"	: true
+	, "network": {
+		"ssid": ""
+		, "pass": ""
+		, "type": ""
+	}
+	, "owner": {
+		"owner":""
+		, "email":""
+		, "first":""
+	}
+	, "lamp": {
+		"temp"	: 1.0
+		, "lum" 	: 1.0
+	}
+}
