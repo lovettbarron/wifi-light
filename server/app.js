48d483bc8b2cacef456ec70ed4396c4936bc31e7
Andrew Lovett-Barron <andrew@readywater.ca>
2012-10-10 12:05:43 -0400

    Query

--- server/app.js
+++ server/app.js
@@ -65,7 +65,7 @@ app.get('/ssid', function(req,res) {
   exec('iwlist wlan0 scanning | grep ESSID'
     ,  function (error, stdout, stderr) {
       if(error) console.log("Err: " + error + stderr);
-      ssid = stdout.regex("/\"[^\"]+\"/");
+      ssid = JSON.stringify(stdout).regex("/\"[^\"]+\"/");
   });
   
   console.log(ssid);
