fb919fcadc0e8e77731970986d9499a34754e4fb
Andrew Lovett-Barron <andrew@readywater.ca>
2012-11-01 21:36:46 -0400

    Numerous changes to client

--- server/public/js/setup.js
+++ server/public/js/setup.js
@@ -8,36 +8,53 @@ $(document).ready( function(){
 	$('.collapse-toggle').collapse();
 	$('#settings').tab('show');
 
+	$('#xypad').css('min-height',$(window).innerHeight()-80);
 
 	$.getJSON('/status', function(data) {
 		console.log(data);
 		status = data;
 		$('.alarmClock').find('.active').removeClass('active');
-		$('#alarm' + data.alarm.time ).addClass('active')
+		$('.alarmClock select').val(data.alarm.time);
 		$('.alarmTime').html( data.alarm.time + ":00" );
+
+
+	$('#xypad').css({
+			'background-color' : getColor(status.lamp.temp,status.lamp.lum/255)
+		});
+
+		var string = (lum * temp / 100) + 'watts/hour';
+		if(e.pageY > buffer + 40) {
+			$('#lens').css( {
+				'left' : e.pageX-50
+				, 'top' : e.pageY-buffer*2
+				, 'background-image': 'radial-gradient(center center, 60px 70px, rgba(0,0,0,.1), rgba(0,0,0,.6));'
+			}).html(string);
+		}
+
+
 		});
 
 	openDrawer(false);
 
 	$('a.setAlarm').click( function(e) {
 		e.preventDefault;
-		var alarmTime = $('.alarmClock').val();
+		console.log("Saving alarm time");
+		var alarmTime = $('.alarmClock select').val();
 		$.getJSON('/alarm/' + alarmTime, function(data) {
 			console.log(data);
 		});
+		$('.alarmTime').html( alarmTime + ":00" );
 	})
 
 
 	$('#xypad').mousemove( function(e) {
-		var string;
-		var buffer = 40;
-		if( e.pageY >= buffer)
-			var lum = Math.floor(255 * ( ( e.pageY)/ $(window).innerHeight()));
+		var buffer = 80;
+		if( e.pageY >= buffer-$(window).innerHeight())
+			var lum = 255-Math.floor(255 * ( ( e.pageY)/ $(window).innerHeight()));
 		else var lum = 0;
 		var temp =  Math.floor(255 * ( e.pageX / $(window).innerWidth()));
 
 
-		string = (lum * temp / 100) + 'watts/hour'
 
 		if(lum < 0) {
 			lum = 0;
@@ -46,14 +63,7 @@ $(document).ready( function(){
 			active = true;
 		}
 
-		$.getJSON('/temp/' + temp, function(data) {
-					console.log(data);
-				});
-
-
-		$.getJSON('/lum/' + lum, function(data) {
-					console.log(data);
-				});
+		changeColor(temp, lum);
 
 
 
@@ -61,13 +71,14 @@ $(document).ready( function(){
 			'background-color' : getColor(temp,lum/255)
 		});
 
+		var string = (lum * temp / 100) + 'watts/hour';
 		if(e.pageY > buffer + 40) {
 			$('#lens').css( {
 				'left' : e.pageX-50
 				, 'top' : e.pageY-buffer*2
+				, 'background-image': 'radial-gradient(center center, 60px 70px, rgba(0,0,0,.1), rgba(0,0,0,.6));'
 			}).html(string);
 		}
-
 
 	});
 
@@ -122,15 +133,27 @@ function openDrawer(open) {
 }
 
 
+function changeColor(temp, lum) {
+		$.getJSON('/temp/' + temp, function(data) {
+				console.log(data);
+			});
+
+
+		$.getJSON('/lum/' + lum, function(data) {
+					console.log(data);
+				});
+}
+
+
 function getColor(temp, lum) {
 	var color;
 	if( lum <= 0. ) {
 		color = 'rgba(0,0,0,1.)';	
 	} else
 	if(temp < 127) {
-		color = 'rgba(50,255,' + Math.floor( 255- (250 * (temp / 255))) + ',' + (1-lum) + ')';
+		color = 'rgba(50,0,' + Math.floor( 255- (250 * (temp / 255))) + ',' + (1-lum) + ')';
 	} else {
-		color = 'rgba(' + Math.floor(200 * (temp / 255) ) + ',255,' + Math.floor(250 * (temp / 255)) + ',' + (1-lum) + ')';
+		color = 'rgba(' + Math.floor(200 * (temp / 255) ) + ',0,' + Math.floor(250 * (temp / 255)) + ',' + (1-lum) + ')';
 	}
 	console.log(color)
 	return color;
