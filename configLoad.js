1ac8bf5a5de37e8b8035dd23a14b46735f482b62
Andrew Lovett Barron <andrewlb@Andrews-MacBook-Pro.local>
2013-01-06 15:54:29 -0500

    Modified shell scripts and config path.

--- configLoad.js
+++ configLoad.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var dir = '/mnt/setting/owl';
+var dir = '/mnt/settings/owl';
 var config = fs.readFileSync(dir + '/config.js');
 console.log(config.toString());
 module.exports = JSON.parse(config);
