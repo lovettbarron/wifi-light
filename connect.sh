16f0cc76954517a933e0fcbbedfe0a3d8356c757
Andrew <andrew@readywater.ca>
2013-01-06 21:35:42 +0000

    Modifying connect.sh

--- connect.sh
+++ connect.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-if ["$1" = 'wpa']
+if [ "$1" = 'wpa' ]
 then
-( 	echo update_config=1; 
+	( echo update_config=1; 
 	echo ctrl_interface=/var/run/wpa_supplicant; 
 	echo network={
 	echo ssid=\"$2\"
-	echo psk=\"$2\" 
+	echo psk=\"$3\" 
 	echo }
-	) >> /etc/wpasupplicant/wpa_supplicant.conf
+	) > /etc/wpa_supplicant/wpa_supplicant.conf
 	wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
 	dhclient wlan0
 
-	return 1
-else if ["$1" ='wep']
+	exit 0
+elif [ "$1" ='wep' ]
 then
 	ifconfig wlan0 up
 	iwconfig wlan0 essid $2 key $3
 	dhclient wlan0
-	return 1
+	exit 0
 else
-	return 0
-	fi
+	exit 1
 fi
