deae3b225156778a9d538e712f1af43dfc7c8334
Andrew Lovett Barron <andrewlb@Andrews-MacBook-Pro.local>
2013-01-06 18:06:50 -0500

    import script

--- configLoad.js
+++ configLoad.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
-var dir = '.';
-fs.existsSync('/mnt/settings/owl', function (exists) {
-  dir = exists ? '/mnt/settings/owl' : '.';
-});
+var dir = '/mnt/settings/owl';
+// fs.existsSync('/mnt/settings/owl', function (exists) {
+//   dir = exists ? '/mnt/settings/owl' : '.';
+// });
 var config = fs.readFileSync(dir + '/config.js');
 console.log(config.toString());
 module.exports = JSON.parse(config);
