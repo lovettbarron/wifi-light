e61718cd54a9471e36e36bdf10473f39b57b1f60
Andrew Lovett Barron <andrewlb@Andrews-MacBook-Pro.local>
2013-01-06 17:13:33 -0500

    Trying to fix an error in the arduino code

--- endAdhoc.sh
+++ endAdhoc.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
+# Kill wifi
+
 echo -n "Killing DHCP server... "
 killall dnsmasq && echo "OK"
 echo -n "Killing wireless... "
 # restoring the wlan interface to "default" mode
-ifconfig $mywlan down
-iwconfig $mywlan mode managed
-iwconfig $mywlan essid off
-iwconfig $mywlan key off
+ifconfig wlan0 down
+iwconfig wlan0 mode managed
+iwconfig wlan0 essid off
+iwconfig wlan0 key off
 echo "OK"
 echo "Wireless Ad-hoc mode terminated."
 
